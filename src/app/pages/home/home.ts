693b55e70f8feae90e0204bab54a35d69f3e0a39
Tonaxe <ymennana@elpuig.xeill.net>
2025-06-04 19:48:59 +0200

    V004: Header scss and html finished.

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { Footer } from '../../shared/footer/footer';
 import { CommonModule } from '@angular/common';
+import { Header } from '../../shared/header/header';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
-  imports: [CommonModule, Footer]
+  imports: [CommonModule, Footer, Header]
 })
 export class Home {}
